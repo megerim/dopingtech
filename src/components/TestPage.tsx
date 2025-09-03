78039dd8849ecc33c02b2e3f419a4b3c377014cd
Mehmet Erim Gökhan <mehmeterimgokhan99@gmail.com>
2025-09-03 19:25:48 +0300

    added the missing ui elements of the test page

--- src/components/TestPage.tsx
+++ src/components/TestPage.tsx
@@ -6,12 +6,48 @@ import styles from '../styles/modules/TestPage.module.scss';
 export const TestPage = () => {
   return (
     <Layout>
-      <div className={styles.testPage}>
-        <div className={styles.questionArea}>
-          <QuestionCard />
-        </div>
-        <div className={styles.opticArea}>
-          <Optic />
+      <div className={styles.testPageContainer}>
+        <header className={styles.header}>
+          <div className={styles.headerLeft}>
+            <button className={styles.backButton}>
+              <img src="/dh/arrow-left.svg" alt="Back" />
+            </button>
+            <h1 className={styles.pageTitle}>Konu Tarama Testi #1</h1>
+          </div>
+
+          <div className={styles.headerRight}>
+            <div className={styles.toggleContainer}>
+              <span>Cevapları Göster</span>
+              <button className={styles.switch}>
+                <span className={styles.switch__slider} />
+              </button>
+            </div>
+            <button className={styles.endTestButton}>
+              <div className={styles.endTestButton__icon}>
+                <img src="/dh/shutdown.png" alt="End Test" />
+              </div>
+              <span>Testi Bitir</span>
+            </button>
+          </div>
+        </header>
+
+        <div className={styles.sectionsWrapper}>
+          <div className={styles.questionArea}>
+            <QuestionCard />
+            <div className={styles.navigationButtons}>
+              <button className={styles.navButton}>
+                <img src="/dh/chevron-left.svg" alt="Previous" />
+                <span>Önceki Soru</span>
+              </button>
+              <button className={styles.navButton}>
+                <span>Sonraki Soru</span>
+                <img src="/dh/chevron-right.svg" alt="Next" />
+              </button>
+            </div>
+          </div>
+          <div className={styles.opticArea}>
+            <Optic />
+          </div>
         </div>
       </div>
     </Layout>
