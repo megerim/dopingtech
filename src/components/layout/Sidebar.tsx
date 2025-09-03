b4317ed4f7c9223a79fda9692afefde0bef32f5c
Mehmet Erim Gökhan <mehmeterimgokhan99@gmail.com>
2025-09-03 18:40:50 +0300

    Icons only sidebar implemented

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,20 +3,30 @@ import styles from '../../styles/modules/Sidebar.module.scss';
 export const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
-      <div>
-        Doping Hafıza
+      <div className={styles.logo}>
+        <img alt="Logo" src="/dh/logo.png" />
       </div>
-      <nav className={styles.nav}>
-        <a href="#link1" className={styles.navLink}>
-          Link 1
-        </a>
-        <a href="#link2" className={styles.navLink}>
-          Link 2
-        </a>
-        <a href="#link3" className={styles.navLink}>
-          Link 3
-        </a>
-      </nav>
+
+      <div className={styles.mainNav}>
+        <div className={`${styles.navIcon} ${styles['navIcon--grid']}`}>
+          <img alt="Layout grid" src="/dh/layout-grid.svg" />
+        </div>
+        <div className={`${styles.navIcon} ${styles['navIcon--book']}`}>
+          <img alt="Book" src="/dh/book.svg" />
+        </div>
+        <div className={`${styles.navIcon} ${styles['navIcon--list']}`}>
+          <img alt="Layout list" src="/dh/layout-list.svg" />
+        </div>
+        <div className={`${styles.navIcon} ${styles['navIcon--check']}`}>
+          <img alt="File check" src="/dh/file-check.svg" />
+        </div>
+        <div className={`${styles.navIcon} ${styles['navIcon--inspect']}`}>
+          <img alt="Inspect" src="/dh/inspect.svg" />
+        </div>
+        <div className={`${styles.navIcon} ${styles['navIcon--activity']}`}>
+          <img alt="Activity" src="/dh/activity.svg" />
+        </div>
+      </div>
     </aside>
   );
 };
