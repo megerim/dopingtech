8482ee1fde77ce634144f08a42c4fc9a08c48c74
Mehmet Erim Gökhan <mehmeterimgokhan99@gmail.com>
2025-09-05 11:02:09 +0300

    Sidebar update, created the layout of icons, created  each icon with its gradient

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -9,22 +9,45 @@ export const Sidebar = () => {
 
       <div className={styles.mainNav}>
         <div className={`${styles.navIcon} ${styles['navIcon--grid']}`}>
-          <img alt="Layout grid" src="/dh/layout-grid.svg" />
+          <img alt="Anasayfa" src="/dh/layout-grid.svg" />
         </div>
         <div className={`${styles.navIcon} ${styles['navIcon--book']}`}>
-          <img alt="Book" src="/dh/book.svg" />
+          <img alt="Dersler" src="/dh/book.svg" />
         </div>
         <div className={`${styles.navIcon} ${styles['navIcon--list']}`}>
-          <img alt="Layout list" src="/dh/layout-list.svg" />
+          <img alt="Soru Bankası" src="/dh/layout-list.svg" />
         </div>
         <div className={`${styles.navIcon} ${styles['navIcon--check']}`}>
-          <img alt="File check" src="/dh/file-check.svg" />
+          <img alt="Deneme Sınavları" src="/dh/file-check.svg" />
         </div>
         <div className={`${styles.navIcon} ${styles['navIcon--inspect']}`}>
-          <img alt="Inspect" src="/dh/inspect.svg" />
+          <img alt="Rehberlik Videoları" src="/dh/inspect.svg" />
         </div>
         <div className={`${styles.navIcon} ${styles['navIcon--activity']}`}>
-          <img alt="Activity" src="/dh/activity.svg" />
+          <img alt="İstatistik" src="/dh/activity.svg" />
+        </div>
+        <div className={`${styles.navIcon} ${styles['navIcon--special']}`}>
+          <img alt="Ölçme Değerlendirme" src="/dh/vr.png" />
+        </div>
+      </div>
+
+      {/* Applications */}
+      <div className={styles.applications}>
+        <div className={`${styles.appIcon} ${styles['appIcon--cozucu']}`}>
+          <img alt="Çözücü App" src="/dh/sidebarCozucu.png" />
+        </div>
+        <div className={`${styles.appIcon} ${styles['appIcon--simdi']}`}>
+          <img alt="Şimdi Anladım App" src="/dh/simdianladim.png" />
+        </div>
+        <div className={`${styles.appIcon} ${styles['appIcon--kocum']}`}>
+          <img alt="Koçum Yanımda App" src="/dh/kocum.png" />
+        </div>
+      </div>
+
+      {/* Feedback */}
+      <div className={styles.feedbackSection}>
+        <div className={styles.feedbackIcon}>
+          <img alt="Görüş Öneri" src="/dh/message-square.svg" />
         </div>
       </div>
     </aside>
