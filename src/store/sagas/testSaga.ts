261ec992309ab500dde6baca3577311c8abb960d
Mehmet Erim Gökhan <mehmeterimgokhan99@gmail.com>
2025-09-05 19:37:12 +0300

    bug fix, type mistake

--- src/store/sagas/testSaga.ts
+++ src/store/sagas/testSaga.ts
@@ -1,7 +1,7 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { loadTestFailure, loadTestSuccess, Question } from '../slices/testSlice';
 
-function fetchQuestionsApi(): Promise<{ questions: Question[] }> {
+function fetchQuestionsApi(): Promise<Question[]> {
   return fetch('/data/questions.json').then((res) => {
     if (!res.ok) throw new Error(`HTTP ${res.status}`);
     return res.json();
@@ -10,8 +10,8 @@ function fetchQuestionsApi(): Promise<{ questions: Question[] }> {
 
 function* loadTestWorker(): Generator<any, any, any> {
   try {
-    const data: { questions: Question[] } = yield call(fetchQuestionsApi);
-    yield put(loadTestSuccess(data.questions));
+    const questions: Question[] = yield call(fetchQuestionsApi);
+    yield put(loadTestSuccess(questions));
   } catch (err: any) {
     yield put(loadTestFailure(err?.message ?? 'Failed to load questions'));
   }
